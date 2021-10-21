e4a2372130551b1cf52d50fcec1a1aba272b32ab
Kavya00 <92917890+Kavya00@users.noreply.github.com>
2021-10-21 16:24:22 +0530

    Create swappingwithouttemp.c

---
+++ swappingwithouttemp.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+
+int a,b;
+printf("Enter value of a and b\n");
+scanf("%d%d",&a,&b);
+a=a+b;
+b=a-b;
+a=a-b;
+printf("Before swapping a=%d,b=%d\n",b,a);
+printf("After swapping a=%d,b=%d\n",a,b);
+ return 0;
+}
